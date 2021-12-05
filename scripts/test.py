5091966a97239e7adf4bf01ce82a405824607a1f
Nicolás Agustín Rodriguez Castro <nicorodc@gmail.com>
2021-12-05 16:34:00 -0300

    jupytext and test notebooks
    Adding jupytext to the environment with a first notebook to test if tensorflow was installed correctly and jupytext.yml was set correctly.

---
+++ scripts/test.py
@@ -0,0 +1,34 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Test
+#
+# This is a test notebook to see if tensorflow and jupytext are installed correctly.
+
+# %%
+import tensorflow as tf
+
+# %%
+tf.test.is_built_with_cuda()
+
+# %%
+tf.test.is_built_with_cuda()
+
+# %% [markdown]
+#
+# More test for jupytext.
+
+# %% [markdown]
+# And even more testing.
